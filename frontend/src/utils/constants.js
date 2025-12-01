42275a2b3ab4d9e5b1013ba337f45bb679e85ea9
Vaishnavi Tapasvi <vaishnavi.tapasvi@gmail.com>
2025-12-01 12:47:58 +0530

    .

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,8 +1,2 @@
-//export const BASE_URL = "http://localhost:5000"
-/*export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "http://backend:5000"   // inside Docker network
-    : "http://localhost:5000"; // when running locally*/
-
 export const BASE_URL = import.meta.env.VITE_API_URL;
     
